2f90863f60926246c14924efeb00df4ed4cc07c4
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-04-05 15:57:54 +0530

    Added simple example

---
+++ src/About.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet";
+
+export default function AboutPage() {
+    return <div>
+        <Helmet>
+            <title>
+                About OneChat
+            </title>
+        </Helmet>
+        This the about page
+    </div>
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,22 @@
-import './App.css'
-import reactLogo from './assets/react.svg'
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import Image from './components/Image'
-import { Link } from 'react-router-dom'
-import { Helmet } from 'react-helmet'
+import { useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <Helmet>
         <title>Main Page of the app</title>
       </Helmet>
       <div>
-        <Link to="https://vitejs.dev" target="_blank">
-          <Image src={viteLogo} className="logo" alt="Vite logo" />
-        </Link>
-        <Link to="https://reactjs.org" target="_blank">
-          <Image src={reactLogo} className="logo react" alt="React logo" />
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+        <Link to="/about">
+          Link to about page
         </Link>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
