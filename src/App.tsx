292d24c31d3fb922037b256c5ac12e6b90e214fe
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-05-24 19:49:26 +0530

    Using yarn for CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <Helmet>
-        <title>Main Page of the app</title>
-      </Helmet>
-      <div>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <Link to="/about">
-          Link to about page
-        </Link>
-      </div>
+      <Layout>
+        <p>This is a test statement</p>
+      </Layout>
     </div>
   );
 }
