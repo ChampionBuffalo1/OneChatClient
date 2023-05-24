c189495b9d01808b5cf2c776113a14beb9d06291
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-05-24 19:54:24 +0530

    Disable the unicorn prefer

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import router from './routes';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+// eslint-disable-next-line unicorn/prefer-query-selector
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
