5715a1a5d8fceed357e8ce8aef86e94e0212f4d8
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-05-24 16:31:05 +0530

    Added login and register routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,8 @@
 import './styles/global.css';
 import React from 'react';
+import router from './routes';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import App from './App';
-import About from './pages/About';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  {
-    path: '/about',
-    element: <About />
-  }
-]);
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
