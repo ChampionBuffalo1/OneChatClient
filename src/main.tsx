cac4d24f44880c24038736f8440fff03258566d9
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-06-18 21:03:38 +0530

    Added redux, helmet and tanstack-query providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,22 @@ import './styles/global.css';
 import React from 'react';
 import router from './routes';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { reduxStore } from './lib/store';
 
+const queryClient = new QueryClient();
 // eslint-disable-next-line unicorn/prefer-query-selector
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={reduxStore}>
+          <RouterProvider router={router} />
+        </Provider>
+      </QueryClientProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
