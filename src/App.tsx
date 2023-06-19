32bb37845f6a83dd24350a1213a7bc92722b588b
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-06-19 17:41:30 +0530

    Added loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { Helmet } from 'react-helmet';
-import { Link } from 'react-router-dom';
-import Layout from './components/Layout';
+import { useEffect } from 'react';
+import { setToken } from './lib/reducers/token';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from './lib/hooks';
 
 function App() {
+  const token = useAppSelector(state => state.token.value);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      const localToken = localStorage.getItem('token');
+      if (localToken) {
+        dispatch(setToken(localToken));
+        navigate('/home');
+      } else {
+        navigate('/login');
+      }
+    }
+  });
+
   return (
-    <div className="App">
-      <Layout>
-        <p>This is a test statement</p>
-      </Layout>
+    <div className="flex justify-center items-center h-screen">
+      <p className="animate-bounce text-3xl bold">OneChat Loading...</p>
     </div>
   );
 }
