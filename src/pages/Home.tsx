e583173cd5b9b95973b93cd5a6fe2694c1177d32
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-10-08 16:10:59 +0530

    Unfinished home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,99 @@
-import { useQuery } from '@tanstack/react-query';
-import { axiosInstance } from '../lib/api';
-import { useState } from 'react';
+import ws from '../lib/ws';
+import Group from './Group';
+import Modal from 'react-modal';
+import { Helmet } from 'react-helmet-async';
+import { useEffect, useState } from 'react';
 import GroupIcon from '../components/GroupIcon';
-import { Link } from 'react-router-dom';
-
-const userGroups = [
-  {
-    id: 1,
-    name: 'Friends',
-    members: ['John', 'Jane', 'Alice']
-  },
-  {
-    id: 2,
-    name: 'Work',
-    members: ['Mike', 'Sarah', 'Tom']
-  },
-  {
-    id: 3,
-    name: 'Family',
-    members: ['David', 'Emily', 'Mark']
-  }
-];
+import GroupForm from '../components/GroupForm';
+import { addGroup } from '../lib/reducers/groups';
+import { useAppDispatch, useAppSelector } from '../lib/hooks';
 
 export default function Home() {
-  const [searchKey, setSearchKey] = useState<string>('');
-  // const [] = useState<string>('');
-  const { isLoading, data, isError, error } = useQuery(['key'], {
-    queryFn: async () => (await axiosInstance.post('/@me')).data,
-    retry: 3
-  });
+  const [isOpen, setIsOpen] = useState(false);
+  const group = useAppSelector(state => state.groups.value);
+  const [searchKey, setSearchKey] = useState('');
+  const [showGroup, setShowGroup] = useState('');
+  const dispatch = useAppDispatch();
+
+  const data = useAppSelector(state => state.groups.value);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      ws.registerListener(event => {
+        const g = JSON.parse(event.data);
+        if (g.data) dispatch(addGroup(g.data.Group));
+      }, token);
+    }
+  }, []);
 
   return (
-    <div className="flex h-screen bg-slate-950">
-      <div className="w-1/5 justify-center text-center">
-        <div className="pt-4">
-          <input
-            type="text"
-            className="input max-w-lg rounded-md mb-4 focus:border-blue-500"
-            placeholder="Search"
-            value={searchKey}
-            onChange={event => setSearchKey(event.target.value)}
-          />
-          <ul className="space-y-4">
-            {userGroups
-              .filter(g => (searchKey ? g.name.toLowerCase().startsWith(searchKey) : true))
-              .map(group => (
-                <li key={group.id}>
-                  <Link to={`/group/${group.id}`} className="space-x-2 text-lg ">
-                    <GroupIcon name={group.name} recentMessage="This is the greatest message" />
-                  </Link>
-                </li>
-              ))}
-          </ul>
+    <>
+      <Helmet>
+        <title>OneChat</title>
+      </Helmet>
+      <div className="flex h-screen bg-slate-950">
+        <div className="lg:w-1/6 min-w-fit justify-center text-center">
+          <div className="pt-4">
+            <input
+              type="text"
+              className="input mx-2 max-w-sm lg:max-w-lg rounded-md mb-4 focus:ring-blue-500 focus:ring-1"
+              placeholder="Search"
+              value={searchKey}
+              onChange={event => setSearchKey(event.target.value)}
+            />
+            <ul className="space-y-4">
+              {group
+                .filter(g => (searchKey ? g.name.toLowerCase().startsWith(searchKey) : true))
+                .map(group => (
+                  <li key={group.id}>
+                    <button
+                      className="space-x-2 text-lg"
+                      onClick={() => {
+                        setShowGroup(group.id);
+                      }}
+                    >
+                      <GroupIcon name={group.name} recentMessage="Loading..." />
+                    </button>
+                  </li>
+                ))}
+            </ul>
+            {/* New Group Modal */}
+            <button onClick={() => setIsOpen(true)}>New Group</button>
+            <div className="flex justify-center">
+              <Modal
+                isOpen={isOpen}
+                onRequestClose={() => setIsOpen(false)}
+                contentLabel="New Group"
+                style={{
+                  content: {
+                    width: 'max-content',
+                    height: 'max-content',
+                    top: '30%',
+                    left: '40%',
+                    background: 'white'
+                  },
+                  overlay: {
+                    opacity: '0.8',
+                    backgroundColor: 'gray'
+                  }
+                }}
+              >
+                <GroupForm close={() => setIsOpen(false)} />
+              </Modal>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 p-4 bg-gray-800">
+          <pre>{JSON.stringify(data, undefined, 2)}</pre>
+          {!showGroup && (
+            <>
+              <h1 className="text-3xl font-bold mb-4">Welcome to the Chat Application!</h1>
+              <p>This is the content area where the chat will be displayed.</p>
+            </>
+          )}
+          {showGroup && <Group id={showGroup} />}
         </div>
       </div>
-      <div className="flex-1 p-4 bg-gray-800">
-        <h1 className="text-3xl font-bold mb-4">Welcome to the Chat Application!</h1>
-        <p>This is the content area where the chat will be displayed.</p>
-      </div>
-    </div>
+    </>
   );
 }
