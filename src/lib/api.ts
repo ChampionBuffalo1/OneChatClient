44737b523470d1b1beb75aeb251f42f086776f75
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-10-08 16:10:24 +0530

    Added content-type header

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:3001/api/v1',
-  maxRedirects: 2
-});
-
-axiosInstance.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  maxRedirects: 2,
+  transformRequest: [
+    (data, header) => {
+      const token = localStorage.getItem('token');
+      header.set('Content-Type', 'application/json');
+      if (token) header.set('Authorization', `Bearer ${token}`);
+      return data;
+    }
+  ]
 });
