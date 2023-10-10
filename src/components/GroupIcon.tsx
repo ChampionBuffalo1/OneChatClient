0763ad1a6afdef813e8848e37d4d20c7f4758bd7
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-10-10 18:33:23 +0530

    Using a different default icon

--- src/components/GroupIcon.tsx
+++ src/components/GroupIcon.tsx
@@ -1,19 +1,27 @@
-export default function Group({ icon, name, recentMessage }: { icon?: string; name: string; recentMessage?: string }) {
+import { Users } from 'lucide-react';
+
+interface GroupIconProps {
+  icon?: string;
+  name: string;
+  recentMessage?: string;
+}
+const RECENT_LIMIT = 20;
+
+export default function GroupIcon({ icon, name, recentMessage }: GroupIconProps) {
   return (
-    // border-gray-300
-    <div className="flex items-center p-4 space-x-4 border-b">
-      <div className="flex items-center justify-center w-12 h-12 bg-gray-200 rounded-full">
-        {icon ? (
-          <img src={icon} alt="Group Icon" className="w-6 h-6" />
-        ) : (
-          <span className="text-gray-900 text-xl">+</span>
-        )}
+    <div className="flex items-center p-4 space-x-2 border-b">
+      <div className="flex items-center justify-center w-12 h-12 rounded-full border border-white">
+        {icon ? <img src={icon} alt="Group Icon" className="w-6 h-6" /> : <Users />}
       </div>
       <div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center mx-2">
           <h2 className="text-md font-semibold">{name}</h2>
         </div>
-        {recentMessage && <p className="text-gray-400">{recentMessage}</p>}
+        {recentMessage && (
+          <p className="text-gray-400 mx-2">
+            {recentMessage.substring(0, RECENT_LIMIT) + (recentMessage.length > RECENT_LIMIT ? '...' : '')}
+          </p>
+        )}
       </div>
     </div>
   );
