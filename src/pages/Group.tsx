426d1004b227a289f6d6257b926ba5da14f61e87
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-10-11 21:04:40 +0530

    Disabling the button if on message

--- src/pages/Group.tsx
+++ src/pages/Group.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import { Send } from 'lucide-react';
+import { axiosInstance } from '../lib/api';
 import Message from '../components/Message';
 import GroupHeader from '../components/GroupHeader';
+import { addMessage } from '../lib/reducers/groups';
 import { useMutation } from '@tanstack/react-query';
 import { useAppDispatch, useAppSelector } from '../lib/hooks';
-import { addMessage } from '../lib/reducers/groups';
 
 export default function Group({ id }: { id: string }) {
   const dispatch = useAppDispatch();
+  const [messageState, setMessageState] = useState<string>('');
+
   // TODO: Complete this
   const mutation = useMutation({
     mutationFn: async (): Promise<Record<string, string>> => {
-      return {};
+      const { data } = await axiosInstance.post(
+        '/group/message',
+        JSON.stringify({
+          groupId: id,
+          content: messageState
+        })
+      );
+      return data;
     },
     onSuccess: ({ data }) => {
+      
       dispatch(addMessage(data));
+    },
+    onError: error => {
+      console.log(error);
     }
   });
 
@@ -26,22 +41,32 @@ export default function Group({ id }: { id: string }) {
       <div className="flex-grow overflow-auto mt-12">
         {/* TODO: Add pagination support once the oldest message has been read */}
         {group.messages.length === 0 ? (
-          <p>Be the first one to start a conversation</p>
+          <p className="text-3xl pt-2 ml-2">Be the first one to start a conversation</p>
         ) : (
           group.messages.map(msg => <Message data={msg} key={msg.id} />)
         )}
       </div>
 
       <div className="flex ml-2 p-4">
-        <input type="text" name="Message" id="message" className="w-full input outline-none" />
-        <div
-          className="flex items-center"
+        <input
+          type="text"
+          name="Message"
+          id="message"
+          value={messageState}
+          className="w-full input outline-none"
+          onChange={e => {
+            setMessageState(e.target.value);
+          }}
+        />
+        <button
+          className="flex items-center disabled:opacity-40 disabled:cursor-not-allowed"
           onClick={() => {
             mutation.mutateAsync();
           }}
+          disabled={!messageState || mutation.isLoading}
         >
           <Send className="mx-4 w-fit" size={28} />
-        </div>
+        </button>
       </div>
     </div>
   );
