8279293a2edf34e16167276b699059e1ff01c284
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-12-28 22:02:28 +0530

    Added toast support

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import './styles/global.css';
 import React from 'react';
 import router from './routes';
-import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import { reduxStore } from './lib/store';
 import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { reduxStore } from './lib/store';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 const queryClient = new QueryClient();
 // eslint-disable-next-line unicorn/prefer-query-selector
@@ -14,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
+        <ToastContainer autoClose={2000} closeOnClick draggable theme="dark" />
         <Provider store={reduxStore}>
           <RouterProvider router={router} />
         </Provider>
