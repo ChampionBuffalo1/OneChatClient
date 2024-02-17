db0038edc4588947133a9143b2c060052f38b202
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-02-17 21:46:34 +0530

    Added user slice to the store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './reducers/user';
 import groupSlice from './reducers/groups';
+import { configureStore } from '@reduxjs/toolkit';
 
 export const reduxStore = configureStore({
   reducer: {
+    user: userSlice,
     groups: groupSlice
   }
 });
