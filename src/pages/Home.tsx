6b56f149d2e9c97a56c7ede80c336f5af55d7bb9
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-06 17:34:02 +0530

    feat: Added websocket authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,50 @@
 import GroupUI from '@/components/GroupUI';
 import { Helmet } from 'react-helmet-async';
-import { useEffect, useState } from 'react';
 import { useAppDispatch } from '../lib/hooks';
 import { setUser } from '@/lib/reducers/user';
+import { useNavigate } from 'react-router-dom';
 import GroupSidebar from '@/components/GroupSideBar';
+import { useToast } from '@/components/ui/use-toast';
+import { useContext, useEffect, useState } from 'react';
+import { SocketContext } from '@/components/socket-provider';
 import InformativeBanner from '@/components/InformativeBanner';
 
 export default function Home() {
-  const [groupId, setGroupId] = useState<string>('');
+  const { toast } = useToast();
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const socket = useContext(SocketContext);
+  const [groupId, setGroupId] = useState<string>('');
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user') || '{}');
-    dispatch(setUser(user));
-    // TODO: connect to websocket and handle events
-  }, [dispatch]);
+    if (!dispatch || !socket) return;
+    const userInfo = localStorage.getItem('user');
+    if (userInfo) {
+      const user = JSON.parse(userInfo || '{}');
+      dispatch(setUser(user));
+    }
+    const token = localStorage.getItem('token');
+    if (token) {
+      socket.registerEvent('USER_AUTH_INIT', () => {
+        socket.sendMessage({ token: 'oki' });
+        socket.registerEvent('USER_META_DATA', (data: Record<string, unknown>) => {
+          // TODO: Finish this
+          console.debug('We got the metadata:', data);
+        });
+        socket.registerEvent('USER_AUTH_FAILURE', () => {
+          toast({
+            variant: 'default',
+            title: 'Invalid Token',
+            description: 'Your token has expired. Please login again'
+          });
+          setTimeout(() => {
+            localStorage.clear();
+            navigate(0);
+          }, 1000 * 5); // 5s delay
+        });
+      });
+    }
+  }, [toast, socket, navigate, dispatch]);
 
   return (
     <>
