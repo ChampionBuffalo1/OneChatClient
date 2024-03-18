711ac0b2f354bb0d13b969ed227180c2c0865fc0
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-18 12:14:56 +0530

    feat: Finishing group store

--- src/lib/reducers/groups.ts
+++ src/lib/reducers/groups.ts
@@ -1,6 +1,6 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const MAX_MESSAGE_PER_GROUP = 100;
+type DateString = string;
 
 export type MessageType = {
   id: string;
@@ -8,19 +8,34 @@ export type MessageType = {
   author: {
     id: string;
     username: string;
-    avatarUrl: string;
+    avatarUrl?: string;
+  };
+  createdAt: DateString;
+  updatedAt?: DateString;
+};
+
+export type MessagePayload = MessageType & {
+  group: {
+    id: string;
+    name: string;
+    iconUrl: string | null;
+    description: string;
   };
 };
 
-type GroupType = {
-  name: string;
+export type GroupType = {
   id: string;
+  name: string;
+  iconUrl?: string;
+  description?: string;
   messages: MessageType[];
-  owner: {
+  owner?: {
     id: string;
     username: string;
+    avatarUrl: string | null;
   };
 };
+
 export interface GroupStore {
   value: GroupType[];
 }
@@ -29,42 +44,86 @@ const initialState: GroupStore = {
   value: []
 };
 
+const MAX_MESSAGE_LIMIT = 40;
+
 export const GroupSlice = createSlice({
   name: 'group',
   initialState,
   reducers: {
+    /**
+     * Adds group to redux store
+     */
     addGroup: (state, action: PayloadAction<GroupType | GroupType[]>) => {
       if (Array.isArray(action.payload)) {
-        state.value.push(...action.payload);
+        for (const group of action.payload) state.value.push(group);
       } else {
         state.value.push(action.payload);
       }
     },
+    /**
+     * Removes the group from the store using group id
+     * @param action `id` of the group
+     */
     removeGroup: (state, action: PayloadAction<string>) => {
-      const index = state.value.findIndex(group => group.id === action.payload);
-      if (index === -1) return;
-      state.value.splice(index, 1);
+      const id = state.value.findIndex(group => group.id === action.payload);
+      if (id !== -1) {
+        state.value.splice(id, 1);
+      }
     },
-    addMessage: (
+
+    updateGroup: (state, action: PayloadAction<string>) => {},
+
+    addMessage: (state, action: PayloadAction<MessagePayload>) => {
+      const group = state.value.find(group => group.id === action.payload.group.id);
+      if (!group) return;
+      if (group.messages.length >= MAX_MESSAGE_LIMIT) {
+        // NOTE: This will cause problem if fetching older messages and adding them into store
+        // which may happen when the user scrolls up at the message UI part
+        group.messages.shift();
+      }
+      group.messages.push(action.payload);
+    },
+
+    removeMessage: (state, action: PayloadAction<Pick<MessagePayload, 'id' | 'group'>>) => {
+      const group = state.value.find(group => group.id === action.payload.group.id);
+      if (!group) return;
+      const messageIdx = group.messages.findIndex(msg => msg.id === action.payload.id);
+      if (messageIdx !== -1) {
+        group.messages.splice(messageIdx, 1);
+      }
+    },
+
+    updateMessage: (state, action: PayloadAction<MessagePayload>) => {
+      const group = state.value.find(group => group.id === action.payload.group.id);
+      if (!group) return;
+      const messageIdx = group.messages.findIndex(msg => msg.id === action.payload.id);
+      if (messageIdx !== -1) {
+        group.messages[messageIdx] = action.payload;
+      }
+    },
+
+    changeIcon: (
       state,
       action: PayloadAction<{
         id: string;
-        message: MessageType;
+        url: string;
       }>
     ) => {
-      const index = state.value.findIndex(group => group.id === action.payload.id);
-      if (index === -1) return;
-      const messages = state.value[index].messages;
-      if (messages.length >= MAX_MESSAGE_PER_GROUP) {
-        // Assuming the messages are sorted by time
-        // Removing the oldest message from store to keep memory usage down
-        messages.shift();
-      }
-      messages.push(action.payload.message);
+      const group = state.value.find(group => group.id === action.payload.id);
+      if (!group) return;
+      group.iconUrl = action.payload.url;
+    },
+
+    /**
+     * Resets the state of group
+     */
+    reset: state => {
+      state.value = initialState.value;
     }
   }
 });
 
-export const { addGroup, addMessage,removeGroup } = GroupSlice.actions;
+export const { addGroup, changeIcon, removeGroup, addMessage, removeMessage, updateMessage, updateGroup, reset } =
+  GroupSlice.actions;
 
 export default GroupSlice.reducer;
