526478b0a5f4508e2a44221dc27773ec2be2f8c4
ChampionBuffalo1 <vidish7nov@gmail.com>
2024-03-22 00:04:11 +0530

    feat: Using objects for storing permissions

--- src/lib/reducers/permissions.ts
+++ src/lib/reducers/permissions.ts
@@ -1,19 +1,19 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export type PermissionData = {
-  groupId: string;
-  userId: string;
+  id: string;
   permission: number;
 };
 
 type InitialStates = {
-  value: PermissionData[];
+  // Group ID => Permission
+  value: Record<string, number>;
 };
 
 const initialState: InitialStates = {
-  value: []
+  value: {}
 };
-
+// Only stores logged in user's permission for each group
 export const permSlice = createSlice({
   name: 'permission',
   initialState,
@@ -21,17 +21,11 @@ export const permSlice = createSlice({
     reset: state => {
       state.value = initialState.value;
     },
-
     setPermission: (state, action: PayloadAction<PermissionData>) => {
-      state.value.push(action.payload);
+      state.value[action.payload.id] = action.payload.permission;
     },
     deletePermission: (state, action: PayloadAction<Omit<PermissionData, 'permission'>>) => {
-      const permission = state.value.findIndex(
-        perm => perm.userId === action.payload.userId && perm.groupId === action.payload.groupId
-      );
-      if (permission !== -1) {
-        state.value.splice(permission, 1);
-      }
+      delete state.value[action.payload.id];
     }
   }
 });
